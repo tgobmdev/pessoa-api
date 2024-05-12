c6289eb4d8e018c7369a9fcf9c5d7604f8b75034
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2024-05-12 05:51:32 -0300

    refactor: improves readability and clarity in AddressService

--- src/main/kotlin/tgobmdev/pessoaapi/service/AddressService.kt
+++ src/main/kotlin/tgobmdev/pessoaapi/service/AddressService.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import tgobmdev.pessoaapi.api.zipcode.client.ZipcodeClient
 import tgobmdev.pessoaapi.component.AddressComponent
+import tgobmdev.pessoaapi.entity.AddressEntity
 import tgobmdev.pessoaapi.exception.ApiException
 import tgobmdev.pessoaapi.mapper.AddressMapper
 import tgobmdev.pessoaapi.message.MessageEnum
@@ -16,23 +17,25 @@ class AddressService(
     private val zipCodeClient: ZipcodeClient,
     private val addressMapper: AddressMapper,
     private val addressComponent: AddressComponent
-
 ) {
+    private fun loadAddressEntityById(addressId: Long): AddressEntity {
+        return addressComponent.findAddressById(addressId)
+            .orElseThrow { throw ApiException.of(404, MessageEnum.CODE_2) }
+    }
+
     fun fetchAllAddresses(): List<AddressDetailsResponse> {
         val addressEntities = addressComponent.findAllAddresses()
         return addressMapper.toAddressDetailsResponseList(addressEntities)
     }
 
     fun fetchAddress(addressId: Long): AddressDetailsResponse {
-        val addressEntity = addressComponent.findAddressById(addressId)
-            .orElseThrow { throw ApiException.of(404, MessageEnum.CODE_2) }
+        val addressEntity = loadAddressEntityById(addressId)
         return addressMapper.toAddressDetailsResponse(addressEntity)
     }
 
     @Transactional(readOnly = true)
     fun fetchAllPersonsByAddressId(addressId: Long): AddressInfoWithPeopleResponse {
-        val addressEntity = addressComponent.findAddressById(addressId)
-            .orElseThrow { throw ApiException.of(404, MessageEnum.CODE_2) }
+        val addressEntity = loadAddressEntityById(addressId)
         return addressMapper.toAddressInfoWithPeopleResponse(addressEntity)
     }
 

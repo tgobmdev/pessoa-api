f8cffaed993d3437d77099589866470501040523
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2024-05-12 04:02:26 -0300

    fix script

--- src/main/resources/db/migration/V1__create_initial_tables.sql
+++ src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -1,33 +1,32 @@
+CREATE SEQUENCE sq_address
+    START WITH 1
+    INCREMENT BY 1;
+
+CREATE SEQUENCE sq_person
+    START WITH 1
+    INCREMENT BY 1;
+
 CREATE TABLE address (
-    id bigint NOT NULL,
+    id bigint NOT NULL DEFAULT nextval('sq_address'),
     city varchar(255),
     state varchar(255),
     state_shortname varchar(255),
     street varchar(255),
     street_number varchar(255),
     zipcode varchar(255),
-    PRIMARY KEY (id))
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE person (
-    id bigint NOT NULL,
+    id bigint NOT NULL DEFAULT nextval('sq_person'),
     date_of_birth date,
     name_person varchar(255),
-    PRIMARY KEY (id))
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE person_address (
     person_id bigint NOT NULL,
-    address_id bigint NOT NULL)
---
-CREATE SEQUENCE sq_address
-    START WITH 1
-    INCREMENT BY 1
-
-CREATE SEQUENCE sq_person
-    START WITH 1
-    INCREMENT BY 1
---
-ALTER TABLE IF EXISTS person_address
-    ADD CONSTRAINT FKcyc1krsxqelkm4uwh65avij23 FOREIGN KEY (address_id) REFERENCES address
-
-ALTER TABLE IF EXISTS person_address
-    ADD CONSTRAINT FKnndfs0btabect8upo03uwgfxt FOREIGN KEY (person_id) REFERENCES person
+    address_id bigint NOT NULL,
+    FOREIGN KEY (person_id) REFERENCES person(id),
+    FOREIGN KEY (address_id) REFERENCES address(id)
+);
